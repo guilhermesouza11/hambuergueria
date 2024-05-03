d0d737cc919b49b6c4ede6edf5b863a5e0fcbb93
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2024-05-03 17:28:15 -0300

    Entendendo o v-model e Data Biding do Vue.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const MyNameApp = {
     data(){
         return {
-            name: "Guilherme",
+            name: "",
             age: 26,
             input_name: ""
         }
     },
-    method: {
+    methods: {
         submitForm(e) {
 
-            e.preventDefault();
+            e.preventDefault(); // Isso aqui bloqueia a ação padrão do evento, não envia o formulário.
             
-            console.log("Aqui!")
+            console.log(this.input_name);
+
+            this.name = this.input_name; // Aqui o valor de 'name' é difinido como 'input_name', 'name' é onde está sendo exibido o valor digitado pelo usuário em 'input_name'. 
         }
     }
 }
