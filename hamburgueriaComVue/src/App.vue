47ca72ba1fd0902bf1c7f5fc47e17e8c55a19986
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2024-05-06 17:59:02 -0300

    Começando a entender sobre subcomponentes

--- hamburgueriaComVue/src/App.vue
+++ hamburgueriaComVue/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <PrimeiroComponente />
   <LifeCycle />
+  <UmaPessoa />
 </template>
 
 <script>
 import LifeCycle from './components/LifeCycle.vue'
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import UmaPessoa from './components/UmaPessoa.vue'
 
 export default {
   name: 'App',
   components: {
     PrimeiroComponente,
-    LifeCycle
+    LifeCycle,
+    UmaPessoa
   }
 }
 </script>
---
+++ hamburgueriaComVue/src/components/InfoPessoal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento!</p>
+    <p>Estou usando as seguintes tecnologias:</p>
+    <ul>
+      <li>HTML</li>
+      <li>Css</li>
+      <li>Javascript</li>
+      <li>VueJS</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'InfoPessoal',
+  data() {}
+}
+</script>
